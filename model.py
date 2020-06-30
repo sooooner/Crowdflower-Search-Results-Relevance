bd7a9891ad520f420b8bd6cc1703301cf8c134e8
sooooner <tnsgh0101@gmail.com>
2020-06-30 15:31:48 +0900

    add gridcv

--- model.py
+++ model.py
@@ -1,4 +1,6 @@
 #-*- coding:utf-8 -*-
+import os
+import dill as pickle
 import numpy as np
 import pandas as pd
 import nltk
@@ -10,6 +12,7 @@ from nltk.corpus import stopwords
 from scipy.sparse import hstack
 
 from sklearn.svm import SVC
+from sklearn.base import clone
 from sklearn.metrics import classification_report, roc_curve, mean_squared_error, auc, confusion_matrix, make_scorer
 from sklearn.pipeline import FeatureUnion
 from sklearn.decomposition import TruncatedSVD
@@ -25,91 +28,11 @@ from imblearn.pipeline import Pipeline
 from utility.utility import *
 from utility.processing import processer
 
+from joblib import Parallel, delayed
 
-def kf_cv(X, Y, train_idx, dev_idx, param_grid):
-    train, dev = X.iloc[train_idx], X.iloc[dev_idx]
-    y, y_dev = Y.iloc[train_idx], Y.iloc[dev_idx]
 
-    train_query = list(train.apply(lambda x:'%s' % x['query_preprocessed'], axis=1))
-    train_title = list(train.apply(lambda x:'%s' % x['product_title_preprocessed'], axis=1))
 
-    dev_query = list(dev.apply(lambda x:'%s' % x['query_preprocessed'], axis=1))
-    dev_title = list(dev.apply(lambda x:'%s' % x['product_title_preprocessed'], axis=1))
 
-    stop_words = text.ENGLISH_STOP_WORDS.union(['http','www','img','border','color','style','padding','table',\
-                                                'font', '', 'thi','inch','ha','width','height',\
-                                                '0','1','2','3','4','5','6','7','8','9'])
-
-    stop_words = text.ENGLISH_STOP_WORDS.union(set(stopwords.words('english')))
-    tfv = text.TfidfVectorizer(min_df=param_grid['min_df'],  max_features=None, strip_accents='unicode', analyzer='word',\
-                               token_pattern=r'\w{1,}', ngram_range=(1, 3), use_idf=True, smooth_idf=True, \
-                               sublinear_tf=True, stop_words = stop_words).fit(train_query + train_title)
-
-    X_train = hstack([tfv.transform(train_query), tfv.transform(train_title)])
-    X_dev = hstack([tfv.transform(dev_query), tfv.transform(dev_title)])
-
-    sim = similarlity_stack()
-    svd = TruncatedSVD(n_components = param_grid['n_components'])
-    scl = StandardScaler(with_mean=False)
-    smt = SMOTETomek(tomek=TomekLinks(sampling_strategy='majority'))
-    svm = SVC(C=param_grid['C'], gamma=param_grid['gamma'], class_weight=param_grid['class_weight'], \
-              kernel=param_grid['kernel'], probability=True)
-
-    X_sim_train = sim.fit_transform(X_train, y)
-    X_svd_train = svd.fit_transform(X_train)
-    X_stacked_train = hstack([X_svd_train, X_sim_train])
-    X_scaled_train = scl.fit_transform(X_stacked_train)
-
-    X_sim_dev = sim.transform(X_dev)       
-    X_svd_dev = svd.transform(X_dev)
-    X_stacked_dev = hstack([X_svd_dev, X_sim_dev])
-    X_scaled_dev = scl.transform(X_stacked_dev)
-
-    X_samp, y_samp = smt.fit_sample(X_scaled_train, y)
-
-    svm_result = svm.fit(X_samp, y_samp)
-    svm_pred_dev = svm_result.predict(X_scaled_dev)
-    svm_pred_proba_dev = svm_result.predict_proba(X_scaled_dev)
-
-    return metric.quadratic_weighted_kappa(y_dev, svm_pred_dev), metric.pr_auc_score(y_dev, svm_pred_proba_dev)
-
-def gridsearchcv(X, Y, parmas):
-    kf = StratifiedKFold(n_splits=5, shuffle=True)
-    parmas = list(product(*parmas))
-    
-    param_list = []
-    cv_kappa_score_mean, cv_kappa_score_std = [], []
-    cv_pr_auc_score_mean, cv_pr_auc_score_std = [], []
-    
-    for param in parmas:
-        start = time.time()
-        # n_components, C, gamma, class_weight, kernel, min_df
-        param_grid = {'n_components' : param[0], 'C' : param[1], \
-                      'gamma' : param[2], 'class_weight' : param[3], \
-                      'kernel' : param[4], 'min_df' : param[5]}
-        cv_kappa_scores, cv_pr_auc_scores = [], []
-        
-        for train_idx, dev_idx in kf.split(X, Y):
-            kappa, pr_auc = kf_cv(X, Y, train_idx, dev_idx, param_grid)
-            cv_kappa_scores.append(kappa)
-            cv_pr_auc_scores.append(pr_auc)
-            
-        param_list.append(param_grid)
-        cv_kappa_score_mean.append(np.mean(cv_kappa_scores))
-        cv_kappa_score_std.append(np.std(cv_kappa_scores))
-        cv_pr_auc_score_mean.append(np.mean(cv_pr_auc_scores))
-        cv_pr_auc_score_std.append(np.std(cv_pr_auc_scores))
-
-        print(param_grid, "kappa_score : %.2f, pr_auc_scores : %.2f"% (np.mean(cv_kappa_scores), np.mean(cv_pr_auc_scores)))
-        print("time : %s"%(str(datetime.timedelta(seconds=int(time.time() - start)))))  
-        
-    results = pd.DataFrame({'param' : param_list, \
-                            'kappa_score_mean' : cv_kappa_score_mean, \
-                            'kappa_score_std' : cv_kappa_score_std, \
-                            'pr_auc_score_mean' : cv_pr_auc_score_mean, \
-                            'pr_auc_score_std' : cv_pr_auc_score_std})
-    
-    return results
 
 def main():
     train = pd.read_csv('./data/preprocessed_train.csv')
@@ -123,13 +46,8 @@ def main():
     test_query = list(test.apply(lambda x:'%s' % x['query_preprocessed'], axis=1))
     test_title = list(test.apply(lambda x:'%s' % x['product_title_preprocessed'], axis=1))
 
-    slicer = int(len(train) * 0.9)
-    y_dev = y[slicer:]
-    y = y[:slicer]
 
-    dev_query = train_query[slicer:]
     train_query = train_query[:slicer]
-    dev_title = train_title[slicer:]
     train_title = train_title[:slicer]
 
     stop_words = text.ENGLISH_STOP_WORDS.union(['http','www','img','border','color','style','padding','table','font', \
@@ -137,20 +55,18 @@ def main():
     stop_words = text.ENGLISH_STOP_WORDS.union(set(stopwords.words('english')))
 
     tfv = text.TfidfVectorizer(min_df=3,  max_features=None, strip_accents='unicode', analyzer='word',token_pattern=r'\w{1,}', \
-                          ngram_range=(1, 3), use_idf=True, smooth_idf=True, sublinear_tf=True, stop_words = stop_words)
+                               ngram_range=(1, 3), use_idf=True, smooth_idf=True, sublinear_tf=True, stop_words = stop_words)
 
     full_data = train_query + train_title
     tfv.fit(full_data)
 
     X_train = hstack([tfv.transform(train_query), tfv.transform(train_title)])
-    X_dev = hstack([tfv.transform(dev_query), tfv.transform(dev_title)])
     X_test = hstack([tfv.transform(test_query), tfv.transform(test_title)])
 
     sim = similarlity_stack()
     svd = TruncatedSVD()
     scl = StandardScaler(with_mean=False)
-    sampling = OneSidedSelection()
-    # sampling = SMOTE()
+    sampling = SVMSMOTE(svm_estimator=SVC(), k_neighbors=3)
     svm =  SVC(probability=True)
 
     clf = Pipeline([('FeatureUnion', FeatureUnion( [('svd', svd), ('sim', sim)] )),\
@@ -158,12 +74,12 @@ def main():
                     ('sampling', sampling),\
                     ('svm', svm)])
                     
-    svd__n_components = [200, 230, 250]
-    sampling__sampling_strategy = ['auto', 'majority']
-    # sampling__sampling_strategy = ['minority ', 'auto']
+    svd__n_components = [230, 250]
+    sampling__sampling_strategy = ['not majority']
     svm__gamma = ['auto', 'scale']
+    svm__kernel = ['rbf', 'poly']
     svm__class_weight = ['balanced', None]
-    svm__C = [10, 100]
+    svm__C = [1, 10, 100, 1000]
 
     scoring = {'kappa': make_scorer(metric.quadratic_weighted_kappa, greater_is_better = True), \
                'pr_auc': make_scorer(metric.pr_auc_score, greater_is_better = True, needs_proba=True, average='micro')}
@@ -172,13 +88,12 @@ def main():
                   'sampling__sampling_strategy' : sampling__sampling_strategy,\
                   'svm__gamma' : svm__gamma,\
                   'svm__class_weight' : svm__class_weight,\
+                  'svm__kernel' : svm__kernel,\
                   'svm__C': svm__C}
                
-    import os
-    cv = os.cpu_count()
-    if cv > 5:
-        cv = 5
-    model = GridSearchCV(estimator=clf, param_grid=param_grid, scoring=scoring, refit='kappa', verbose=10, n_jobs=-1, iid=True, cv=cv)
+
+    cv = StratifiedKFold(n_splits=5, shuffle=True)
+    model = GridSearchCV(estimator=clf, param_grid=param_grid, scoring=scoring, refit='pr_auc', verbose=10, n_jobs=-1, iid=True, cv=cv)
     model.fit(X_train, y)
 
     results = pd.DataFrame(model.cv_results_)
@@ -201,36 +116,176 @@ def main():
     sampled_svm_grid_submission = pd.DataFrame({"id": idx, "prediction": model_preds})
     if not os.path.exists("./submission"):
         os.makedirs("./submission")
-    sampled_svm_grid_submission.to_csv("./submission/imbalance_svm.csv", index=False)
+    sampled_svm_grid_submission.to_csv("./submission/imbalance_svmsmote.csv", index=False)
     
     print(confusion_matrix(y_dev, dev_preds))
     print(classification_report(y_dev, dev_preds))
     plot_multiclass_roc_prc(best_model, X_dev, y_dev, save=True)
 
 
-if __name__=="__main__":
-    df_train = pd.read_csv('./data/preprocessed_train.csv')
-    Y = df_train['median_relevance']
-    test = pd.read_csv('./data/preprocessed_test.csv')
-    idx = test.id.values.astype(int)
 
+def _fit(clf, X, Y, train_idx, dev_idx, param_grid, stop_words, i):
+    '''
+    return scaled data
+    '''
+    print('Fitting %d model'%i)
+    train, dev = X.iloc[train_idx], X.iloc[dev_idx]
+    y, y_dev = Y.iloc[train_idx], Y.iloc[dev_idx]
+    
+    train_query = list(train.apply(lambda x:'%s' % x['query_preprocessed'], axis=1))
+    train_title = list(train.apply(lambda x:'%s' % x['product_title_preprocessed'], axis=1))
+
+    dev_query = list(dev.apply(lambda x:'%s' % x['query_preprocessed'], axis=1))
+    dev_title = list(dev.apply(lambda x:'%s' % x['product_title_preprocessed'], axis=1))
+    
+    tfv = text.TfidfVectorizer(min_df=param_grid['min_df'],  max_features=None, strip_accents='unicode', analyzer='word',\
+                               token_pattern=r'\w{1,}', ngram_range=(1, 3), use_idf=True, smooth_idf=True, \
+                               sublinear_tf=True, stop_words = stop_words).fit(train_query + train_title)
+
+    X_train = hstack([tfv.transform(train_query), tfv.transform(train_title)])
+    X_dev = hstack([tfv.transform(dev_query), tfv.transform(dev_title)])
+    
+    X_scaled_train = clf.fit_transform(X_train)
+    X_scaled_dev = clf.transform(X_dev)
+    
+    return X_scaled_train, y, X_scaled_dev, y_dev
+
+
+def _sampling(smt, X, Y, i):
+    '''
+    return sampled data
+    '''
+    print('sampling %d model'%i)
+    X_samp, y_samp = smt.fit_sample(X, Y)
+    return X_samp, y_samp 
+
+
+def _fit_and_score(svm, sampled, scaled, i):
+    '''
+    return kappa, auc score
+    '''
+    print('Calculating score %d model'%i)
+    X_samp, y_samp  = sampled
+    X_scaled_dev, y_dev = scaled
+    
+    svm_result = svm.fit(X_samp, y_samp)
+    svm_pred_dev = svm_result.predict(X_scaled_dev)
+    svm_pred_proba_dev = svm_result.predict_proba(X_scaled_dev)
+    return metric.quadratic_weighted_kappa(y_dev, svm_pred_dev), metric.pr_auc_score(y_dev, svm_pred_proba_dev)
+
+
+def parallel_k_fold(data, target, param, stop_words):
+    '''
+    return cv_kappa_scores, cv_pr_auc_scores, param_grid
+    '''
     # n_components, C, gamma, class_weight, kernel, min_df
-    # parmas = [[230], [100, 1000], ['auto'], ['balanced', None], ['rbf', 'poly'], [3, 5, 7]]
-    parmas = [[230], [100], ['auto'], [None], ['rbf'], [5]]
+    param_grid = {'n_components' : param[0], 'C' : param[1], \
+                  'gamma' : param[2], 'class_weight' : param[3], \
+                  'kernel' : param[4], 'min_df' : param[5]}
+    
+    n_splits = 5
+    n_jobs = 5
+    start = time.time()
+    _k_fold = StratifiedKFold(n_splits=n_splits, shuffle=True)
+    parallel = Parallel(n_jobs=n_jobs)
+    print('Fitting %d models with'%n_splits, param_grid, 'params %d at a time ...'%n_jobs)
 
-    result = gridsearchcv(df_train, Y, parmas)
-        
-    result.to_csv("./gridsearch/resultss.csv", index=False)
     
+    sim = similarlity_stack()
+    svd = TruncatedSVD(n_components = param_grid['n_components'])
+    clf = Pipeline([('FeatureUnion', FeatureUnion( [('svd', svd), ('sim', sim)] )),\
+                    ('scl', scl)])
     
+    scaled_data = parallel(
+        delayed(_fit)(clone(clf), data, target, train_index, test_index, param_grid, stop_words, i) 
+        for i, (train_index, test_index) in enumerate(_k_fold.split(data, target))
+    )
+    scaled_data = np.array(scaled_data)
+    dev = scaled_data[:, 2:]
+    scaled_data = scaled_data[:, :2]
     
+    smt = SVMSMOTE(sampling_strategy='not majority', svm_estimator=SVC(C=param_grid['C'], gamma=param_grid['gamma']), n_jobs=-1)
+    sampled_data = parallel(
+        delayed(_sampling)(clone(smt), scaled_X, y, i) 
+        for i, (scaled_X, y) in enumerate(scaled_data)
+    )
     
+    del scaled_data
+    svm = SVC(C=param_grid['C'], gamma=param_grid['gamma'], class_weight=param_grid['class_weight'], \
+              kernel=param_grid['kernel'], probability=True)
+    score_list = parallel(
+        delayed(_fit_and_score)(clone(svm), sampled, scaled, i) 
+        for i, (sampled, scaled) in enumerate(zip(sampled_data, dev))
+    )
     
+    cv_kappa_scores, cv_pr_auc_scores = [], []
+    del sampled_data, dev
+    for kappas, aucs in score_list:
+        cv_kappa_scores.append(kappas)
+        cv_pr_auc_scores.append(aucs)
     
+    print('Finished', param_grid, "kappa_score : %.2f, pr_auc_scores : %.2f"% (np.mean(cv_kappa_scores), np.mean(cv_pr_auc_scores)), \
+          'time : %s'%(str(datetime.timedelta(seconds=int(time.time() - start)))))
+    return np.mean(cv_kappa_scores), np.std(cv_kappa_scores), np.mean(cv_pr_auc_scores), np.std(cv_pr_auc_scores), param_grid
+
+
+def gridsearchcv(data, target, parmas):
+    parmas = list(product(*parmas))
     
+    param_list = []
+    cv_kappa_score_mean, cv_kappa_score_std = [], []
+    cv_pr_auc_score_mean, cv_pr_auc_score_std = [], []
     
+    stop_words = text.ENGLISH_STOP_WORDS.union(['http','www','img','border','color','style','padding','table',\
+                                                'font', '', 'thi','inch','ha','width','height',\
+                                                '0','1','2','3','4','5','6','7','8','9'])
+    stop_words = text.ENGLISH_STOP_WORDS.union(set(stopwords.words('english')))
     
+    n_jobs = 2
+    parallel = Parallel(n_jobs=n_jobs)
     
+    scores_statistic = parallel(
+        delayed(parallel_k_fold)(data, target, param, stop_words) 
+        for i, param in enumerate(parmas)
+    )
+
+    for kappa_mean, kappa_std, auc_mean, auc_std, param_grid in scores_statistic:
+        param_list.append(param_grid)
+        cv_kappa_score_mean.append(kappa_mean)
+        cv_kappa_score_std.append(kappa_std)
+        cv_pr_auc_score_mean.append(auc_mean)
+        cv_pr_auc_score_std.append(auc_std)
+          
+    results = pd.DataFrame({'param' : param_list, \
+                            'kappa_score_mean' : cv_kappa_score_mean, \
+                            'kappa_score_std' : cv_kappa_score_std, \
+                            'pr_auc_score_mean' : cv_pr_auc_score_mean, \
+                            'pr_auc_score_std' : cv_pr_auc_score_std})
+    
+    return results
+
+
+
+if __name__=="__main__":
+    # df_train = pd.read_csv('./data/preprocessed_train.csv')
+    # Y = df_train['median_relevance']
+    # df_train = df_train[['query_preprocessed', 'product_title_preprocessed']]
+    # test = pd.read_csv('./data/preprocessed_test.csv')
+    # idx = test.id.values.astype(int)
+    # test = test[['query_preprocessed', 'product_title_preprocessed']]
+    # 
+    # # n_components, C, gamma, class_weight, kernel, min_df
+    # parmas = [[230], [10, 100, 1000], ['auto'], ['balanced', None], ['rbf', 'poly'], [3]]
+    # # parmas = [[230], [100], ['auto'], [None], ['rbf'], [5]]
+    # 
+    # result = gridsearchcv(df_train, Y, parmas)
+    # 
+    # import os
+    # if not os.path.exists("./gridsearch"):
+    #     os.makedirs("./gridsearch")
+    # result.to_csv("./gridsearch/results.csv", index=False)
+    
+    main()
     
     
     
